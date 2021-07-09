81d8bb65f018cbe35af2d8baed247561791bb153
Koitoresu <85047431+Koitoresu@users.noreply.github.com>
2021-07-09 19:43:36 -0400

    Create User.java (#9)

---
+++ User.java
@@ -0,0 +1,14 @@
+package com.google.sps.data;
+
+public class User {
+
+  private String email;
+
+  public User(String email) {
+    this.email = email;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+}
