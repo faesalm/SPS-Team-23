10cc917d14c8d0e80af390dffbeb43feea76b4e7
Koitoresu <torreskoy@oulook.com>
2021-07-13 19:07:32 +0000

    WIP:adding entity logic

--- src/main/java/myapp/SubmissionServlet.java
+++ src/main/java/myapp/SubmissionServlet.java
@@ -14,27 +14,48 @@
  * limitations under the License.
  */
 
-package myapp;
+package com.google.sps.servlets;
 
+import com.google.cloud.datastore.Datastore;
+import com.google.cloud.datastore.DatastoreOptions;
+import com.google.cloud.datastore.Entity;
+import com.google.cloud.datastore.FullEntity;
+import com.google.cloud.datastore.KeyFactory;
 import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
 
 public class SubmissionServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws IOException {
-        System.out.println(req.getParameterNames().toString());
 
-    String username = req.getParameter("full-name");
-    String email = req.getParameter("email-address");
-    String language = req.getParameter("language");
-    String message = req.getParameter("message");
-    System.out.println(username + "--" + email + "--" + language + "--" + message);
-
-    
     // TODO: store in datastore
+
+    String Username = Jsoup.clean(request.getParameter("full-name"), Whitelist.none());
+    String email= Jsoup.clean(request.getParameter("email-address"), Whitelist.none());
+    String message = Jsoup.clean(request.getParameter("message"), Whitelist.none());
+    String language = Jsoup.clean(request.getParameter("language"), Whitelist.none()); 
+
+    Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind("full-name");
+        FullEntity taskEntity =
+        Entity.newBuilder(keyFactory.newKey())
+        .set("full-name", username)
+        .set("email-address", email)
+        .set("language", language)
+        .set("message", message)
+        .build();
+    datastore.put(userEntity);  
+    response.sendRedirect("/index.html");  
+} 
+
+//Work in Prog
+
 
     // resp.setHeader("Content-Type", "text/html; charset=UTF-8");
     // PrintWriter out = resp.getWriter();
